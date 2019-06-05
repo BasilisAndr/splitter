a4014239988c96001ca5cb83fde4e4c10563612f
BasilisAndr <blindedbysunshine@gmail.com>
2019-06-05 23:43:02 +0300

    for heroku

--- app/main.py
+++ app/main.py
@@ -4,13 +4,12 @@ import subprocess, os
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
 @app.route("/", methods=['POST', 'GET'])
 def main_endpoint():
     if request.method == 'POST':
         stri = request.form['input']
-        print(os.listdir('..'))
-        print(os.listdir())
         if stri:
             res = subprocess.check_output(f'echo {stri} | hfst-lookup /dependencies/files/rus.hfstol | python /dependencies/files/post.py | python /dependencies/files/postpost.py', shell=True)
             res = res.decode("utf-8")
@@ -22,7 +21,7 @@ def main_endpoint():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(debug=True)
 
 
 __all__ = app
